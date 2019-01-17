c2bcb5d51d343b581efb2e3f44970313d37acd50
Steve Albonico <salbonico@gmail.com>
2019-01-17 02:35:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
 # code here!
+class School
+
+attr_reader :name,:roster
+
+def initialize(name)
+@name = name
+@roster = {}
+end
+
+def add_student(student, grade)
+  @roster[grade] = student
+end
+
+end
